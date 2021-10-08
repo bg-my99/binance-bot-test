84faffe15b2af8df32cbe4f4bc9f6b2394f48ad0
crispg72 <chris.e.green@hotmail.com>
2021-10-08 18:48:47 +0100

    Simple websocket connection

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/adshao/go-binance/v2"
+)
+
+var (
+	apiKey    = ""
+	secretKey = ""
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	binance.UseTestnet = true
+	wsDepthHandler := func(event *binance.WsDepthEvent) {
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneC, stopC, err := binance.WsDepthServe("LTCBTC", wsDepthHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// use stopC to exit
+	go func() {
+		time.Sleep(5 * time.Second)
+		stopC <- struct{}{}
+	}()
+	// remove this if you do not want to be blocked here
+	<-doneC
 }
