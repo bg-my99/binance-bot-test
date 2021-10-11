67d5172c691cca333f678a91f5255df6120ca7d2
crispg72 <chris.e.green@hotmail.com>
2021-10-11 19:47:18 +0100

    Grab the depth snapshot before opening stream

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -21,6 +22,12 @@ func main() {
 	}
 	fmt.Printf("Connecting to %s\n", whichNet)
 	binance.UseTestnet = cfg.UseTestnet
+
+	client := binance.NewClient(cfg.AccessKeys.ApiKey, cfg.AccessKeys.SecretKey)
+	symbol := "LTCBTC"
+	depthSnapshot, err := client.NewDepthService().Symbol(symbol).Limit(1000).Do(context.Background())
+	fmt.Printf("%v\n", depthSnapshot)
+
 	wsDepthHandler := func(event *binance.WsDepthEvent) {
 
 		fmt.Printf("%s:\n", time.Unix(event.Time/1e3, (event.Time%1e3)*1e6).Format(time.RFC3339))
@@ -36,7 +43,7 @@ func main() {
 	errHandler := func(err error) {
 		fmt.Println(err)
 	}
-	doneC, stopC, err := binance.WsDepthServe("LTCBTC", wsDepthHandler, errHandler)
+	doneC, stopC, err := binance.WsDepthServe(symbol, wsDepthHandler, errHandler)
 	if err != nil {
 		fmt.Println(err)
 		return
