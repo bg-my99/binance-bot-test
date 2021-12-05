c08a8de45a606cbc0fbf5e7452a0e593f17e53ab
crispg72 <chris.e.green@hotmail.com>
2021-12-05 19:17:16 +0000

    Exponential moving averages

--- calcs/calcs.go
+++ calcs/calcs.go
@@ -6,6 +6,37 @@ import (
 	"gonum.org/v1/plot/plotter"
 )
 
+func CalcExpMovingAverages(price float64, currentEMA20 float64, currentEMA50 float64, currentEMA100 float64) (float64, float64, float64) {
+	eMA20Multiplier := (2.0 / (20.0 + 1.0))
+	eMA50Multiplier := (2.0 / (50.0 + 1.0))
+	eMA100Multiplier := (2.0 / (100.0 + 1.0))
+
+	newEMA20 := (price * eMA20Multiplier) + (currentEMA20 * (1 - eMA20Multiplier))
+	newEMA50 := (price * eMA50Multiplier) + (currentEMA50 * (1 - eMA50Multiplier))
+	newEMA100 := (price * eMA100Multiplier) + (currentEMA100 * (1 - eMA100Multiplier))
+
+	return newEMA20, newEMA50, newEMA100
+}
+
+func GetExpMovingAverages(pts plotter.XYs, numPoints int) (plotter.XYs, plotter.XYs, plotter.XYs) {
+	eMA20 := plotter.XYs{}
+	eMA50 := plotter.XYs{}
+	eMA100 := plotter.XYs{}
+
+	runningEMA20 := pts[0].Y
+	runningEMA50 := pts[0].Y
+	runningEMA100 := pts[0].Y
+	for _, trade := range pts {
+
+		runningEMA20, runningEMA50, runningEMA100 = CalcExpMovingAverages(trade.Y, runningEMA20, runningEMA50, runningEMA100)
+
+		eMA20 = append(eMA20, plotter.XY{X: trade.X, Y: runningEMA20})
+		eMA50 = append(eMA50, plotter.XY{X: trade.X, Y: runningEMA50})
+		eMA100 = append(eMA100, plotter.XY{X: trade.X, Y: runningEMA100})
+	}
+	return eMA20, eMA50, eMA100
+}
+
 func GetMovingAverage(pts plotter.XYs, numPoints int) plotter.XYs {
 	movingAverage := plotter.XYs{}
 	runningCount := 0.0
