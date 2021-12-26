ee3965d259fa2b1c46b12f05f15b6ead1ec2ca86
crispg72 <chris.e.green@hotmail.com>
2021-12-26 12:50:03 +0000

    Removed dead code

--- js-client/src/App.js
+++ js-client/src/App.js
@@ -4,30 +4,6 @@ import Select from "react-select";
 import RunPlot from './RunPlot';
 
 
-function UpdateTraces(data, selectedOption) {
-  var traces = []
-  console.log(selectedOption)
-  //for (let line of Object.entries(data.linesByTimestamp["120000000000"].lines)) {
-  for (let line of Object.entries(data.linesByTimestamp[selectedOption].lines)) {
-    var x = []
-    var y = []
-    var z = []
-    for (let point of Object.entries(line[1].points.sort((a, b) => (a.rsiSell > b.rsiSell) ? 1 : -1))) {
-      x.push(line[0])
-      y.push(point[1].rsiSell)
-      z.push(point[1].pnl)
-    }
-    traces.push({
-      x: x,
-      y: y,
-      z: z,
-      type: 'scatter3d',
-      mode: 'lines'  
-    })
-  }
-  return traces
-}
-
 /**
  * Main application component
  *
