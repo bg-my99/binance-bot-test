56bbc6bf9820aded34016469cade943091e8099a
crispg72 <chris.e.green@hotmail.com>
2021-12-26 12:47:42 +0000

    Adapt to moved plotting

--- js-client/src/App.js
+++ js-client/src/App.js
@@ -1,15 +1,41 @@
 import React, { useState, useCallback } from 'react';
-import Plot from 'react-plotly.js'
 import Select from "react-select";
 
+import RunPlot from './RunPlot';
+
+
+function UpdateTraces(data, selectedOption) {
+  var traces = []
+  console.log(selectedOption)
+  //for (let line of Object.entries(data.linesByTimestamp["120000000000"].lines)) {
+  for (let line of Object.entries(data.linesByTimestamp[selectedOption].lines)) {
+    var x = []
+    var y = []
+    var z = []
+    for (let point of Object.entries(line[1].points.sort((a, b) => (a.rsiSell > b.rsiSell) ? 1 : -1))) {
+      x.push(line[0])
+      y.push(point[1].rsiSell)
+      z.push(point[1].pnl)
+    }
+    traces.push({
+      x: x,
+      y: y,
+      z: z,
+      type: 'scatter3d',
+      mode: 'lines'  
+    })
+  }
+  return traces
+}
 
 /**
  * Main application component
  *
  * @returns
  */
-function App() {
+ export const App = () => {
   const [timesteps, setTimesteps] = useState()
+  const [selectedOption, setSelectedOption] = useState()
 
   const [traces, setTraces] = useState(function() {
     fetch("http://localhost:8080/encode").then(function(response) {
@@ -20,30 +46,13 @@ function App() {
         timesteps.push({value: step[0], label: step[0]})
       }
       setTimesteps(timesteps)
+      setTraces(data)
+      //console.log(JSON.stringify(data))
+      setSelectedOption(timesteps[0])
+      //console.log(JSON.stringify(timesteps[0]))
 
-      var traces = []
-      for (let line of Object.entries(data.linesByTimestamp["120000000000"].lines)) {
-        var x = []
-        var y = []
-        var z = []
-        for (let point of Object.entries(line[1].points.sort((a, b) => (a.rsiSell > b.rsiSell) ? 1 : -1))) {
-          x.push(line[0])
-          y.push(point[1].rsiSell)
-          z.push(point[1].pnl)
-        }
-        traces.push({
-          x: x,
-          y: y,
-          z: z,
-          type: 'scatter3d',
-          mode: 'lines'  
-        })
-    }
-      setTraces(traces)
-      //console.log(JSON.stringify(data))    
     }).catch(error => console.log(error.message))
   });
-  const [selectedOption, setSelectedOption] = useState()
 
   const onOptionChange = useCallback((option) => setSelectedOption(option), []);
 
@@ -54,14 +63,7 @@ function App() {
         onChange={onOptionChange}
         options={timesteps}
       />
-      <Plot
-        data={traces}
-        layout={{
-          width: 900,
-          height: 800,
-          title: `Simple 3D Scatter`
-        }}
-      />
+      {(traces && selectedOption) ? <RunPlot traces={traces && traces.linesByTimestamp[selectedOption.value]} /> : ""}
     </div>
   );
 }
